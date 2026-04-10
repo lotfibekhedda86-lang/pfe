545abf1843e7b07e1a56c477dde382d65615b6bc
lotfibekhedda86-lang <lotfibekhedda86@gmail.com>
2026-04-10 22:08:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var cart=document.getElementById('cart');
+
+function openCart(){
+    
+    cart.classList.add('active');
+ 
+}
+function closeCart(){
+    cart.classList.remove('active');
+  
+}
+
+    
